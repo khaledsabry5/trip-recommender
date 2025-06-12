36a1663fbcc760a6d3837843348b46da19c272e1
khaledsabry5 <127055482+khaledsabry5@users.noreply.github.com>
2025-06-12 12:50:19 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
-from models import RecommendationRequest
 from recommendation import recommend_trips
+from models import RecommendationRequest
 
 app = FastAPI()
 
 @app.post("/recommend_trips")
 def get_recommendations(request: RecommendationRequest):
     recommended_trips = recommend_trips(request)
-    return {"user_id": request.user_id, "recommended_trips": recommended_trips}
+    return {"recommended_trips": recommended_trips}
 
 if __name__ == "__main__":
     import uvicorn
